a43cbdd787e5fade8bbf1cbb1f5c79b38e4537a3
jcfr4n <jcfm73@gmail.com>
2021-10-31 09:44:27 +0000

    creado el archivo con ejercicios de variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+# La asignación de valores a las variables es dinámico y no está fuertemente tipado
+
+x = 5
+y = 10
+# Para poder concatenar debo llevarlas a  tipo string
+
+print ("La suma de " + str(x) + " más " + str(y) + " es: " + str(x+y) )
